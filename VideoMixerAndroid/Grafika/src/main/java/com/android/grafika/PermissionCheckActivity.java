a47847c72e3ce4e2d7cc5e8672c109fb21503506
Jack <gskwon@maxst.com>
2017-06-27 22:23:14 +0900

    PermissionActivity 추가

---
+++ VideoMixerAndroid/Grafika/src/main/java/com/android/grafika/PermissionCheckActivity.java
@@ -0,0 +1,41 @@
+package com.android.grafika;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.ArrayList;
+
+public class PermissionCheckActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkPermission();
+    }
+
+    private void checkPermission() {
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                startActivity(new Intent(PermissionCheckActivity.this, MainActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                finish();
+            }
+        };
+
+        new TedPermission(this)
+                .setPermissionListener(permissionlistener)
+                .setPermissions(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE)
+                .check();
+    }
+
+}
